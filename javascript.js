9d4ce638229346d306994e7366dac0ffd33b5a59
Santy2206 <153763536+Santy2206@users.noreply.github.com>
2024-09-06 18:03:34 -0500

    <feat>[Create The Features Section]:<JavaScript Feature> {   1.Make A Mini Icon That When We Switch To The Next Sliper Change Of Color     1.1 Made the same steps as the carrusel but in the stylesheet add     a selector to active state of color white
    }

--- javascript.js
+++ javascript.js
@@ -1,13 +1,28 @@
 const buttons=document.querySelectorAll('[data-carousel-button]')
+
+const carousel=
 buttons.forEach(button => {
   button.addEventListener('click',()=>{
-    const offset=button.dataset.carouselButton==='next'?1:-1
-    const slides=button.closest('[data-carousel]').querySelector('[data-slides]')
-    let activeSlide=slides.querySelector('[data-active]')
-    let newIndex=[...slides.children].indexOf(activeSlide)+offset
-    if(newIndex<0)newIndex=slides.children.length -1
-    if(newIndex>=slides.children.length)newIndex=0
-    slides.children[newIndex].dataset.active=true 
+    function getOffset(){
+      return button.dataset.carouselButton==='next'?1:-1
+
+    }
+    function selectSlides(){
+      return button.closest('[data-carousel]').querySelector('[data-slides]')
+    }
+   
+    const slidesIcon=button.closest('[data-carousel]').querySelector('[data-slides-icon]')
+    let activeSlide=selectSlides().querySelector('[data-active]')
+    let activeIcon=slidesIcon.querySelector('[data-active]')
+    let newIndex=[...selectSlides().children].indexOf(activeSlide)+getOffset()
+    let newIndexIcon=[...slidesIcon.children].indexOf(activeIcon)+getOffset()
+    if(newIndex<0)newIndex=selectSlides().children.length -1
+    if(newIndexIcon<0)newIndexIcon=slidesIcon.children.length -1
+    if(newIndex>=selectSlides().children.length)newIndex=0
+    if(newIndexIcon>=slidesIcon.children.length)newIndexIcon=0
+    selectSlides().children[newIndex].dataset.active=true 
+    slidesIcon.children[newIndexIcon].dataset.active=true 
     delete activeSlide.dataset.active
+    delete activeIcon.dataset.active
   })
 });
