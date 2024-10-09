2f205414cc01555612220e334249d1c7f5a3f2dc
Santy2206 <153763536+Santy2206@users.noreply.github.com>
2024-10-09 15:53:20 -0500

    <feat>[Create The FAQ'S Section]:<JavaScript Features & Make It Responsive> [   1.Create a feature than when click the icon appear the answer     1.1 HTML       -Add a data attribute to the icon  and the answer p     1.2 CSS       - Turn the answer and question to absolute -     - Turn the span to relative     1.3 JavaScript       - Create a listening event in the icons       - for each icon   2.Make it responsive       - Fix the position and font size ]

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const buttons=document.querySelectorAll('[data-carousel-button]')
 
+const iconContainer=document.querySelectorAll('[data-drop-down-icon-container]')
+
 const carouselChangeSlide=buttons.forEach(button => {
   button.addEventListener('click',()=>{
     function getOffset(){
@@ -31,4 +33,63 @@ const carouselChangeSlide=buttons.forEach(button => {
     delete whiteIcon.dataset.white
   })
 })
+let count=0
+const showDropAnswer=iconContainer.forEach(iconContainer => {
+ iconContainer.addEventListener('click',()=>{
+  let activeSlide=iconContainer.closest('[data-drop-down-container]').closest('[data-active]')
+  let dropDownContainer=activeSlide.querySelector('[data-drop-down-container ]')
+  let slides=activeSlide.closest('[data-slides]')
+  let icon=iconContainer.querySelector('[data-drop-down-icon]')
+  let dropDown=dropDownContainer.querySelector('[data-drop-down]')
+  let answer=dropDown.querySelector('[data-answer]')
+  
+  count++
+  let offSet=slides.querySelector('li').dataset.active='true'?1:-1
+  console.log(offSet)
+  let newIndex=[...slides.children].indexOf(activeSlide)+offSet
+  console.log(newIndex)
+
+
+  
+  
+  
+
+   slides.children[newIndex].dataset.active='true'
+   delete activeSlide.dataset.active
+
+   
+
+  if(activeSlide.dataset.active=true){
+    
+  answer.style.zIndex='1'
+  answer.style.opacity='1'
+  answer.style.transition='200ms opacity'
+  answer.style.position='static'
+  icon.style.color='red'
+  icon.classList.replace('fa-caret-right','fa-caret-down')
+  
+
+
+
+  
+
+  }
+  else if(!activeSlide.dataset.active){
+    answer.style.zIndex='-999'
+    answer.style.opacity='0'
+    answer.style.transition='200ms opacity'
+    answer.style.position='absolute'
+    icon.style.color='white'
+    icon.classList.replace('fa-caret-down','fa-caret-right')
+    
+
+  } 
+  
+  
+  
+  
+  
+ })
+});
+
 
